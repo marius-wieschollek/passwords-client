bfecacb1417828c65addccd3b340d4a83693c71f
Marius David Wieschollek <passwords.public@mdns.eu>
2019-08-20 14:52:11 +0200

    Fixed missing data object

--- src/Classes/EnhancedApi.js
+++ src/Classes/EnhancedApi.js
@@ -57,7 +57,7 @@ export default class EnhancedApi extends SimpleApi {
         config.events.on('api.request.failed', (e) => {
             if(e.id && e.id === '4ad27488') this._resetAuthorisation();
         });
-        config.events.on('api.session.token.changed', () => {
+        config.events.on('api.session.token.changed', (d) => {
             if(d.oldSessionToken) this._resetAuthorisation();
         });
 
