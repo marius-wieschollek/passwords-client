4fab8775d898174066f9d3021b3427f8f9ef4e4e
Marius David Wieschollek <passwords.public@mdns.eu>
2020-12-15 23:42:25 +0100

    Improce settings repository
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/Repositories/SettingRepository.js
+++ src/Repositories/SettingRepository.js
@@ -10,60 +10,114 @@ export default class SettingRepository {
         this._converter = api.getInstance(`converter.setting`);
     }
 
+    /**
+     * @returns {Promise<SettingCollection>}
+     */
     async findAll() {
         let response = await this._api.getRequest()
-            .setPath(`1.0/settings/list`)
-            .send();
+                                 .setPath(`1.0/settings/list`)
+                                 .send();
 
         return this._converter.fromApiObject(response.getData());
     }
 
+    /**
+     * @param {String} scope
+     * @returns {Promise<SettingCollection>}
+     */
     async findByScope(scope) {
         let response = await this._api.getRequest()
-            .setPath(`1.0/settings/list`)
-            .setData({scopes: [scope]})
-            .send();
+                                 .setPath(`1.0/settings/list`)
+                                 .setData({scopes: [scope]})
+                                 .send();
 
         return this._converter.fromApiObject(response.getData());
     }
 
+    /**
+     * @param {String[]} scopes
+     * @returns {Promise<SettingCollection>}
+     */
     async findByScopes(scopes) {
         let response = await this._api.getRequest()
-            .setPath(`1.0/settings/list`)
-            .setData({scopes})
-            .send();
+                                 .setPath(`1.0/settings/list`)
+                                 .setData({scopes})
+                                 .send();
 
         return this._converter.fromApiObject(response.getData());
     }
 
+    /**
+     * @param {String} name
+     * @returns {Promise<SettingCollection>}
+     */
     async findByName(name) {
         let response = await this._api.getRequest()
-            .setPath(`1.0/settings/get`)
-            .setData([name])
-            .send();
+                                 .setPath(`1.0/settings/get`)
+                                 .setData([name])
+                                 .send();
 
         return this._converter.fromApiObject(response.getData());
     }
 
+    /**
+     * @param {String[]} names
+     * @returns {Promise<SettingCollection>}
+     */
     async findByNames(names) {
         let response = await this._api.getRequest()
-            .setPath(`1.0/settings/get`)
-            .setData(names)
-            .send();
+                                 .setPath(`1.0/settings/get`)
+                                 .setData(names)
+                                 .send();
         return this._converter.fromApiObject(response.getData());
     }
 
+    /**
+     *
+     * @param {Setting} setting
+     * @returns {Promise<void>}
+     */
     async set(setting) {
-        let response = await this._api.getRequest()
-            .setPath(`1.0/settings/set`)
-            .setData(setting)
-            .send();
+        let key = `${setting.getScope()}.${setting.getName()}`,
+            data = {};
+        data[key] = setting.getValue();
+
+        let response =
+                await this._api.getRequest()
+                          .setPath(`1.0/settings/set`)
+                          .setData(data)
+                          .send();
     }
 
+    /**
+     *
+     * @param {Setting} setting
+     * @returns {Promise<void>}
+     */
     async reset(setting) {
-        let response = await this._api.getRequest()
-            .setPath(`1.0/settings/reset`)
-            .setData(setting)
-            .send();
+        let key = `${setting.getScope()}.${setting.getName()}`;
+        let response =
+                await this._api.getRequest()
+                          .setPath(`1.0/settings/reset`)
+                          .setData([key])
+                          .send();
+    }
+
+    /**
+     *
+     * @param {Setting} setting
+     * @returns {Promise<void>}
+     */
+    update(setting) {
+        return this.set(setting);
+    }
+
+    /**
+     *
+     * @param {Setting} setting
+     * @returns {Promise<void>}
+     */
+    create(setting) {
+        return this.set(setting);
     }
 }
